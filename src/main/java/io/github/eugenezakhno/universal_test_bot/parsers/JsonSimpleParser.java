b3423143f970fe1ae40ec65a6f8d476d896247c4
EugeneZakhno <zohan1234567@gmail.com>
2021-03-07 16:37:36 +0300

    added JsonSimpleParser

--- src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/JsonSimpleParser.java
+++ src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/JsonSimpleParser.java
@@ -1,6 +1,31 @@
 package io.github.eugenezakhno.universal_test_bot.parsers;
 
-public class JsonSimpleParser {
 
+import io.github.eugenezakhno.universal_test_bot.model.Root;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonSimpleParser {
+    public Root parse(){
+        Root root = new Root();
+        JSONParser parser = new JSONParser();
+        try (BufferedReader reader = new BufferedReader(new FileReader("./universal_test_bot/src/main/resources/files/config-bot.json"))){
+        JSONObject rootJsonObject = (JSONObject) parser.parse(reader);
+        String botName = (String) rootJsonObject.get("botName");
+
+        root.setName(name);
+        return root;
+
+        } catch (IOException | ParseException e) {
+            System.err.println("Parsing error" + e.toString());
+        }
+
+        return null;
+    }
 
 }
