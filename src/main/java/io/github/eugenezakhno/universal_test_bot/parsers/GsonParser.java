669990fdf1a26a2bd1b3b5525129e0e39437a2c5
EugeneZakhno <zohan1234567@gmail.com>
2021-03-09 22:24:04 +0300

    deleted while loop into parser

--- src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/GsonParser.java
+++ src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/GsonParser.java
@@ -6,22 +6,18 @@ import io.github.eugenezakhno.universal_test_bot.model.Bot;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
 public class GsonParser {
     String line;
     public Bot parse(){
         Gson gson = new Gson();
-        try (BufferedReader reader = new BufferedReader(new FileReader("P2PCHELINDBANKB118876F775EED2A7C.json"))){
-            while ((line = reader.readLine()) != null) {
-              //  String json_string = "{\"botName\":\"Tom\", \"botToken\": \"Broody\"}";
-                Bot bot = gson.fromJson(line, Bot.class);
-             //   Bot bot1 = gson.fromJson(json_string, Bot.class);
+        try (BufferedReader reader = new BufferedReader(new FileReader("P2PCHELINDBANKB118876F775EED2A7C.json"))) {
+                Bot bot = gson.fromJson(reader, Bot.class);
                 return bot;
-                }
-            } catch (IOException e) {
-            System.err.println("Parsing JSON error" + e.toString());
-        }
+            } catch(IOException e){
+                System.err.println("Parsing JSON error" + e.toString());
+            }
         return null;
+
     }
 }
