f8d12d58ac8f0f004c068f4f61ef9e4166ddea95
EugeneZakhno <zohan1234567@gmail.com>
2021-03-24 00:00:02 +0300

    refactoring UniversalTestBotApplication

--- src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/GsonParser.java
+++ src/main/java/io/github/eugenezakhno/universal_test_bot/parsers/GsonParser.java
@@ -3,8 +3,6 @@ package io.github.eugenezakhno.universal_test_bot.parsers;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.github.eugenezakhno.universal_test_bot.model.Bot;
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -17,7 +15,7 @@ public class GsonParser {
             Bot bot = gson.fromJson(reader, Bot.class);
                 return bot;
             } catch(IOException e){
-                System.err.println("Parsing JSON error " + e.toString());
+                System.err.println(" Parsin g JSON error " + e.toString());
             }
         return null;
 
